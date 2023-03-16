d57f28c48fde35c90cfcbf9e652ca6c194882acd
anuragverma01 <yuvrajverma9890@gmail.com>
2023-03-17 00:23:44 +0530

    first commit

--- src/screen/CreateAccount.js
+++ src/screen/CreateAccount.js
@@ -6,8 +6,6 @@ import gosolar from '../../asset/image/gosolar.png'
 
 import { useNavigation } from '@react-navigation/native';
 
-import Login from "../component/Login";
-
 const CreateAccount = (props) => {
 
     const navigation = useNavigation();
